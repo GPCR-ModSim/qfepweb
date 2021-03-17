9f7c7085d78fb5d9d4c18868bc7b3679a75405d6
Xabi Bello <xbello@gmail.com>
2021-03-17 13:47:34 +0100

    Added a couple of links towards "about" page.

--- about/test_views.py
+++ about/test_views.py
@@ -14,3 +14,11 @@ class Home(TestCase):
         assert page.status_code == 200
 
         assert "<title>About</title>" in page.content.decode()
+
+    def test_about_is_linked_from_navigation(self):
+        page = self.client.get(reverse("home:index"))
+
+        assert reverse("about:index") in page.content.decode()
+
+        # Assert one link in the title, another in the footer
+        self.assertContains(page, reverse("about:index"), count=2)
