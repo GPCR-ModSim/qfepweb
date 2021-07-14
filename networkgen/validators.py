bc8dffd62bb243db1239f2397ec50689d419a015
Jenke <jscheen@MacBook-Pro-e8cc>
2021-07-14 10:08:23 +0100

    check rdkit molecule validity

--- networkgen/validators.py
+++ networkgen/validators.py
@@ -3,15 +3,24 @@ from rdkit.Chem.rdmolfiles import ForwardSDMolSupplier
 
 
 def two_ligands(sdf_io):
-    """Check that the file at "sdf_io" contains at least two molecules."""
+    """Check that the file at "sdf_io" contains at least two molecules 
+    that RDKit is able to read."""
     suppl = list(ForwardSDMolSupplier(sdf_io))
 
+    # check that input SDF contains multiple ligands.
     if len(suppl) == 0:
         raise ValidationError(
-            "SDF file contains no ligands. Needs two at least")
+            "SDF file contains no ligands. Needs two at least.")
 
     if len(suppl) == 1:
         raise ValidationError(
-            "SDF file contains a single ligand. Needs two at least")
+            "SDF file contains a single ligand. Needs two at least.")
+
+    # now check molecule validity. RDKit returns a None molecule when reading fails.
+    # see https://github.com/rdkit/rdkit/issues/642 if we want to extend this test.
+    for i, mol in enumerate(suppl):
+    	if mol == None:
+    		raise ValidationError(
+    			f"RDKit is unable to read molecule with index {i} in the input SDF file.")
 
     sdf_io.seek(0)
