d7e8aef8ca0c5472e5bf9219c5252ec44179406a
Jenke <jscheen@MacBook-Pro-e8cc>
2021-07-14 09:55:36 +0100

    extended sdf check

--- networkgen/validators.py
+++ networkgen/validators.py
@@ -5,7 +5,13 @@ from rdkit.Chem.rdmolfiles import ForwardSDMolSupplier
 def two_ligands(sdf_io):
     """Check that the file at "sdf_io" contains at least two molecules."""
     suppl = list(ForwardSDMolSupplier(sdf_io))
+
+    if len(suppl) == 0:
+        raise ValidationError(
+            "SDF file contains no ligands. Needs two at least")
+
     if len(suppl) == 1:
         raise ValidationError(
-            "SDF with only one ligand. Needs two at least")
+            "SDF file contains a single ligand. Needs two at least")
+
     sdf_io.seek(0)
